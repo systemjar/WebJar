0575882912f191a3c40d5d909a014d349ae3e20e
jorge <jorge@INSPIRON153511>
2024-11-30 11:18:07 -0600

    EntidadEmpresaCompleta

--- WebJar.Backend/Migrations/20241130170954_EmpresaEntityCompleta.Designer.cs
+++ WebJar.Backend/Migrations/20241130170954_EmpresaEntityCompleta.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebJar.Backend.Data;
 
@@ -10,9 +11,11 @@ using WebJar.Backend.Data;
 namespace WebJar.Backend.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20241130170954_EmpresaEntityCompleta")]
+    partial class EmpresaEntityCompleta
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- WebJar.Shared/Entities/Empresa.cs
+++ WebJar.Shared/Entities/Empresa.cs
@@ -17,6 +17,121 @@ namespace WebJar.Shared.Entities
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public string Nombre { get; set; } = null!;
 
+        [Display(Name = "Dirección Empresa")]
+        [MaxLength(65, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Direccion { get; set; } = null!;
+
+        [Display(Name = "Patrono")]
+        [MaxLength(65, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Patrono { get; set; } = null!;
+
+        [Display(Name = "Dirección Patrono")]
+        [MaxLength(65, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string DirecionPatrono { get; set; } = null!;
+
+        [Display(Name = "Número Patronal")]
+        [MaxLength(15, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string NumeroPatronal { get; set; } = null!;
+
+        [Display(Name = "Proceso de producción")]
+        [MaxLength(1, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Produce { get; set; } = null!;
+
+        [Display(Name = "Largo Nivel 1")]
+        [MaxLength(1, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Nivel1 { get; set; } = null!;
+
+        [Display(Name = "Largo Nivel 2")]
+        [MaxLength(1, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Nivel2 { get; set; } = null!;
+
+        [Display(Name = "Largo Nivel 3")]
+        [MaxLength(1, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Nivel3 { get; set; } = null!;
+
+        [Display(Name = "Largo Nivel 4")]
+        [MaxLength(1, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Nivel4 { get; set; } = null!;
+
+        [Display(Name = "Largo Nivel 5")]
+        [MaxLength(1, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Nivel5 { get; set; } = null!;
+
+        [Display(Name = "Largo Nivel 6")]
+        [MaxLength(1, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Nivel6 { get; set; } = null!;
+
+        [Display(Name = "Activo")]
+        [MaxLength(11, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Activo { get; set; } = null!;
+
+        [Display(Name = "Pasivo")]
+        [MaxLength(11, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Pasivo { get; set; } = null!;
+
+        [Display(Name = "Capital")]
+        [MaxLength(11, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Capital { get; set; } = null!;
+
+        [Display(Name = "Ventas")]
+        [MaxLength(11, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Ventas { get; set; } = null!;
+
+        [Display(Name = "Costos")]
+        [MaxLength(11, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Costos { get; set; } = null!;
+
+        [Display(Name = "Gastos")]
+        [MaxLength(11, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Gastos { get; set; } = null!;
+
+        [Display(Name = "Otros Ingresos")]
+        [MaxLength(11, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string OtrosIngresos { get; set; } = null!;
+
+        [Display(Name = "OtrosGastos")]
+        [MaxLength(11, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string OtrosGastos { get; set; } = null!;
+
+        [Display(Name = "Produccion")]
+        [MaxLength(11, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Produccion { get; set; } = null!;
+
+        [Display(Name = "Porcentaje IVA")]
+        [Range(0.01, 100.00, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal PorcentajeIva { get; set; }
+
+        [Display(Name = "NivelPresupuesto")]
+        [MaxLength(1, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string NivelPresupuesto { get; set; } = null!;
+
+        [Display(Name = "Resultado")]
+        [MaxLength(11, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Resultado { get; set; } = null!;
+
         public ICollection<Cuenta>? Cuentas { get; set; }
     }
 }
